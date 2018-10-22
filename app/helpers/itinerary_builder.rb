bd79bb03c2002eede3a3501d29dd5610a7ef934e
Raymond Kwan <rayk199492@gmail.com>
2018-10-22 17:03:32 -0400

    Added helpers to generate itinerary

---
+++ app/helpers/itinerary_builder.rb
@@ -0,0 +1,30 @@
+# This is a helper file that parses API info according to user arguments
+require_relative "./yelp-fusion/yelp-api"
+# Given an object of arguments, return an itinerary JSON object.
+
+def generate_intinerary(info)
+  coord = info.coord
+  start_time = info.start_time
+  end_time = info.end_time
+  time_frame = end_time - start_time
+  itinerary = []
+
+  info.order.each do |type|
+    case type
+    when 'Restaurant'
+      itinerary << yelp_search(coord)
+    when 'Movie'
+      #movie api
+    else
+      #invalid type
+    end
+  end
+end
+
+# Use the google map API to get the route information for the itinerary
+def get_route(itinerary)
+  itinerary.each do |item|
+    #concat to query string
+  end
+  #make query to google map API
+end
