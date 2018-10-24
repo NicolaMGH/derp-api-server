c240dfb0b753c9f7fb567afd07a14d4f9693a3c3
Nicola <va3nky@hotmail.com>
2018-10-24 16:07:03 +0000

    added login error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,8 @@ class SessionsController < ApplicationController
       # logged in when they navigate around our website.
       session[:user_id] = user.id
     else
-    # If user's login doesn't work, send them back to the login form.
       @status = false
+      render json: {'error': 'login incorrect'}
     end
   end
 
